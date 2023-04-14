d4bc480a6a92e79f841c5a13c884740b41bf9a4c
Md. Abdullah Al Mamun <124173465+fakir0099@users.noreply.github.com>
2023-04-14 15:04:51 +0600

    Create Listing.dart

---
+++ Listing.dart
@@ -0,0 +1,20 @@
+void main() {
+  List list = [
+    {
+      "name": "Yakub",
+      "age": 13,
+      "address": "Dhaka",
+    },
+    {
+      "name": "Nahid",
+      "age": 13,
+      "address": "Dhaka",
+    },
+    {
+      "name": "Porosh",
+      "age": 13,
+      "address": "Dhaka",
+    },
+  ];
+  print(list[0]);
+}
